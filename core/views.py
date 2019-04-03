bb3f60bc2e761e859c83687ed63dcd084ebc759e
mbahamondes <mbahamondes.osorio@gmail.com>
2019-04-03 17:53:34 -0300

    add static files

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('Inicio')
+
+def about(request):
+    return HttpResponse('Historia')
+
+def services(request):
+    return HttpResponse('Servicios')
+
+def contact(request):
+    return HttpResponse('Contacto')
+
+def store(request):
+    return HttpResponse('Visitanos')
+
+def blog(request):
+    return HttpResponse('Blog')
+
+def sample(request):
+    return HttpResponse('Sample')
