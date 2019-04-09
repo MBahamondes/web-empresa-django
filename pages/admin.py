cd7e90ce949527886598115e86219fb2885db189
mbahamondes <mbahamondes.osorio@gmail.com>
2019-04-09 17:47:20 -0400

    add order field to Page models

--- pages/admin.py
+++ pages/admin.py
@@ -3,5 +3,6 @@ from .models import Page
 
 class PageAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at')
+    list_display = ('title', 'order')
 
 admin.site.register(Page, PageAdmin)
